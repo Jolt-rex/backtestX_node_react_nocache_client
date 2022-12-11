f8d0834a3f049a5b5385b24d01256db344127b98
Lee <leepotter010@gmail.com>
2022-12-11 20:13:52 +1000

    Created responsive navbar with bootstrap

---
+++ src/components/NavbarX.jsx
@@ -0,0 +1,28 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+//import './Navbar.css';
+
+const NavbarX = () => {
+  return ( 
+    <Navbar bg="dark" variant="dark" expand="lg" fixed="top" >
+      <Container>
+        <Navbar.Brand href="#" className="nav-link">backtest-X</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#" className="nav-link">Dashboard</Nav.Link>
+            <Nav.Link href="#" className="nav-link">Social</Nav.Link>
+            <Nav.Link href="#" className="nav-link">Search</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#" className="nav-link nav-account">Login</Nav.Link>
+            <Nav.Link href="#" className="nav-link nav-account">Register</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+   );
+}
+ 
+export default NavbarX;
