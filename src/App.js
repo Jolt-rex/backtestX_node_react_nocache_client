86c0e80803663f0884f71f811e7b7e14f3ff6577
Lee <leepotter010@gmail.com>
2022-12-12 16:03:15 +1000

    Added theme and directory structure for components / scenes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import './App.css';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavbarX from './components/NavbarX';
+
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import Topbar from './scenes/global/Topbar';
 
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="App">
-      <NavbarX />
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
