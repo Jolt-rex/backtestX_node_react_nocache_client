c5a26ec3e34c6b91698755721078f95bb6b095f6
Lee <leepotter010@gmail.com>
2022-12-12 19:15:14 +1000

    Added sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Routes, Route } from 'react-router-dom';
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Topbar from './scenes/global/Topbar';
+import Sidebar from './scenes/global/Sidebar';
+import Dashboard from './scenes/dashboard';
+// import Team from './scenes/team';
+// import Favorites from './scenes/favorites';
+// import FAQ from './scenes/faq';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -13,8 +18,15 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              {/* <Route path="/team" element={<Team />} />
+              <Route path="/favorites" element={<Favorites />} />
+              <Route path="/faq" element={<FAQ />} /> */}
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
